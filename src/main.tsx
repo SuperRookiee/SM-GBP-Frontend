7c18d8d36020709dbecb80f2d196d3705f0657c8
Hyunwook Go <hyunwook.go@concentrix.com>
2026-02-09 13:18:36 +0900

    feat: add tanstack query devtool

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./styles/global.css";
 
@@ -19,6 +20,7 @@ createRoot(document.getElementById("root")!).render(
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
                 <App/>
+                {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
             </BrowserRouter>
         </QueryClientProvider>
     </StrictMode>
