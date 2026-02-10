65e99051137f2b93cabeb83259e430250a8b68a2
Hyunwook Go <hyunwook.go@concentrix.com>
2026-02-10 15:15:51 +0900

    chore: add margin in scroll area

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -31,7 +31,7 @@ const ScrollBar = ({
       orientation={orientation}
       className={cn(
         "flex touch-none select-none transition-colors",
-        orientation === "vertical" && "h-full w-2.5 border-l border-l-transparent p-px",
+        orientation === "vertical" && "h-full w-2.5 border-l border-l-transparent p-px my-0.5",
         orientation === "horizontal" && "h-2.5 w-full flex-col border-t border-t-transparent p-px",
         className,
       )}
