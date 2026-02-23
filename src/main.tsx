97f0a9f0a939fec182a3fe0414e9a6b52c1cdcd0
Hyunwook Go <hyunwook.go@concentrix.com>
2026-02-23 15:49:04 +0900

    fix: Add Global TooltipProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { TooltipProvider } from "@/components/ui/tooltip.tsx";
 import App from "./App";
 import "./styles/global.css";
 
@@ -19,10 +20,12 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <App/>
-                {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-            </BrowserRouter>
+            <TooltipProvider delayDuration={0}>
+                <BrowserRouter>
+                    <App/>
+                    {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+                </BrowserRouter>
+            </TooltipProvider>
         </QueryClientProvider>
     </StrictMode>
 );
