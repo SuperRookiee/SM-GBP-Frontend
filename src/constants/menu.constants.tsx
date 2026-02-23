1af5e78a651ac38f4f0dd9f391c135d8dbf1add8
HyunWook Go <89756426+SuperRookiee@users.noreply.github.com>
2026-02-23 15:11:55 +0900

    feat: update GNB structure and place demo on the right

--- src/constants/menu.constants.tsx
+++ src/constants/menu.constants.tsx
@@ -1,12 +1,89 @@
-import { ChartLineIcon, HomeIcon, LifeBuoy, Send, Settings2Icon, ShoppingCartIcon, UserIcon } from "lucide-react";
+import { ChartLineIcon, HomeIcon, LifeBuoy, Send, Settings2Icon, ShoppingCartIcon } from "lucide-react";
 
 export const MENU = {
     navMain: [
         {
+            titleKey: "menu.businessInfo",
+            url: "#",
+            icon: <ChartLineIcon/>,
+            isActive: true,
+            items: [
+                { titleKey: "menu.businessList", url: "#" },
+                { titleKey: "menu.groupManagement", url: "#" },
+                { titleKey: "menu.businessCategoryCreate", url: "#" },
+                { titleKey: "menu.businessInfoCreate", url: "#" },
+            ],
+        },
+        {
+            titleKey: "menu.productInfo",
+            url: "#",
+            icon: <ShoppingCartIcon/>,
+            items: [
+                { titleKey: "menu.productList", url: "#" },
+                { titleKey: "menu.productCategoryCreate", url: "#" },
+                { titleKey: "menu.productInfoCreate", url: "#" },
+            ],
+        },
+        {
+            titleKey: "menu.imageInfo",
+            url: "#",
+            icon: <HomeIcon/>,
+            items: [
+                { titleKey: "menu.imageList", url: "#" },
+                { titleKey: "menu.logoUpload", url: "#" },
+                { titleKey: "menu.coverUpload", url: "#" },
+                { titleKey: "menu.businessImageUpload", url: "#" },
+            ],
+        },
+        {
+            titleKey: "menu.reservationSettings",
+            url: "#",
+            icon: <HomeIcon/>,
+            items: [
+                { titleKey: "menu.reservationList", url: "#" },
+                { titleKey: "menu.reservationSettingsCreate", url: "#" },
+            ],
+        },
+        {
+            titleKey: "menu.postManagement",
+            url: "#",
+            icon: <HomeIcon/>,
+            items: [
+                { titleKey: "menu.postList", url: "#" },
+                { titleKey: "menu.postInfoCreate", url: "#" },
+            ],
+        },
+        {
+            titleKey: "menu.reviewManagement",
+            url: "#",
+            icon: <HomeIcon/>,
+            items: [
+                { titleKey: "menu.reviewList", url: "#" },
+                { titleKey: "menu.reviewRequestCreate", url: "#" },
+            ],
+        },
+        {
+            titleKey: "menu.qnaManagement",
+            url: "#",
+            icon: <HomeIcon/>,
+            items: [
+                { titleKey: "menu.qnaList", url: "#" },
+            ],
+        },
+        {
+            titleKey: "menu.adminSettings",
+            url: "#",
+            icon: <Settings2Icon/>,
+            items: [
+                { titleKey: "menu.adminList", url: "#" },
+                { titleKey: "menu.permissionInfoCreate", url: "#" },
+                { titleKey: "menu.notice", url: "#" },
+            ],
+        },
+        {
             titleKey: "menu.demo",
             url: "#",
             icon: <HomeIcon/>,
-            isActive: true,
             items: [
                 { titleKey: "menu.dataTable", url: "/demo/data_table" },
                 { titleKey: "menu.gridTable", url: "/demo/grid_table" },
@@ -19,26 +96,6 @@ export const MENU = {
                 { titleKey: "menu.notFound", url: "/demo/not_found" },
             ],
         },
-        {
-            titleKey: "menu.sample1",
-            url: "#",
-            icon: <ChartLineIcon/>,
-            items: [
-                { titleKey: "menu.sample11", url: "#" },
-                { titleKey: "menu.sample12", url: "#" },
-            ],
-        },
-        {
-            titleKey: "menu.sample2",
-            url: "#",
-            icon: <ShoppingCartIcon/>,
-            items: [
-                { titleKey: "menu.sample21", url: "#" },
-                { titleKey: "menu.sample22", url: "#" },
-            ],
-        },
-        { titleKey: "menu.user", url: "/user", icon: <UserIcon/> },
-        { titleKey: "menu.settings", url: "#", icon: <Settings2Icon/> },
     ],
     navSecondary: [
         { titleKey: "menu.sampleSecondary1", url: "#", icon: <LifeBuoy/> },
